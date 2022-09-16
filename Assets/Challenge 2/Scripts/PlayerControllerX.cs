175efeb30ab1cc309cd4d4032ed9cb1f255a548c
Michael <seotanbolaji@gmail.com>
2022-09-16 10:32:21 +0100

    Play fetch commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float SpawnTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SpawnTimer > 0)
+        {
+            SpawnTimer -= Time.deltaTime;
+        }
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space ) && SpawnTimer <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            SpawnTimer = 2.0f;
+        }
+    }
+}
